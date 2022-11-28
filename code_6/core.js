a0af4d56056d2a42fac4e693c5bdd097975434ed
hallzyx <hallinxsl7@gmail.com>
2022-11-28 11:01:17 -0500

    Mis primeros pasos con java script

---
+++ code_6/core.js
@@ -0,0 +1,15 @@
+console.log('¡zawardo!')
+        
+
+let numero= 12;
+
+console.log(typeof numero);
+
+let n1="17";
+console.log(parseInt (n1));
+console.log(typeof n1);
+
+let n2=Number(prompt("Ingrese el primer numero:"));
+let n3=Number(prompt("Ingrese el segundo numero:"));
+
+alert(n2+n3);

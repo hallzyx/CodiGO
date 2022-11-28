3ad3c8d27a2bc8835f5ae8201eb4e1179bc2f4eb
hallzyx <hallinxsl7@gmail.com>
2022-11-28 16:48:19 -0500

    Probando el control de flujo try,cach y finally

--- code_6/core.js
+++ code_6/core.js
@@ -1,12 +1,58 @@
 //Declaracion de funcion
-function suma(nx,ny){
-    return nx+ny;
+    
+function centecima(){
+    n=(prompt("Ingresa un numero de 3 cifras"));
+
+    try{
+        if(n.length>3){
+            throw error="MORE";
+        }
+        else if(n.length<3){
+            throw error="LESS";
+        }
+    
+    rpta = "El numero "+n+" es correcto.";
+    
+    }
+    catch(error){
+        if (error=="MORE") {
+            rpta= "EL numero es mayor de 3 cifras"
+            }
+        
+        else if(error=="LESS"){
+            rpta ="El numero es menor de 3 cifras"
+        }
+
+    }
+    
+    
+    finally{
+
+        if(error=="Default"){
+            console.log(rpta);
+        }
+        else{
+            console.error(rpta);
+        }
+        
+    }
 }
 
-let n2=Number(prompt("Ingrese el primer numero:"));
-let n3=Number(prompt("Ingrese el segundo numero:"));
 
 
-let sum=suma(n2,n3);
+let error="Default";
+let n;
+let rpta;
+let n_1;
+let n_2;
+let n_3;
 
-alert("La suma es "+ sum)
+centecima();
+n_1=parseInt(n);
+n_2=parseInt(n)*2;
+n_3=n_1+n_2;
+console.log("La respuesta es " +n_3);
+
+ 
+
+

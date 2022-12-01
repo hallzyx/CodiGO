62576b870eb4bb9df0bf18010d2e971349915353
hallzyx <hallinxsl7@gmail.com>
2022-12-01 10:04:13 -0500

    Navegando entre selectores con js

--- code_9/node.js
+++ code_9/node.js
@@ -1,13 +1,27 @@
 function core(nod,col,fonts,backs,fontf){
     let token;
-    token=document.querySelector(nod);
+    token=document.querySelectorAll(nod);
 
-    token.style.color=col;
-    token.style.fontSize=fonts+"px";
-    token.style.background=backs;
-    token.style.fontFamily=fontf
-}
+    for(let t=0; t<token.length;t++){
+        token[t].style.color=col;
+        token[t].style.fontSize=fonts+"px";
+        token[t].style.background=backs;
+        token[t].style.fontFamily=fontf;
+    }
+    }
+
+   
 
 
 core("h1","red",60,"orange","Arial");
-core("h2","blue",40,"green","Verdana");
+core("h2","blue",40,"yellow","Verdana");
+core("div h1","green",100,"cyan", "calibri")
+
+
+const lista = document.querySelector("ul");
+let n=1;
+const elementos= lista.querySelectorAll("li");
+
+console.log(elementos[2].previousElementSibling);
+
+console.log(elementos[1].parentElement.parentElement.querySelector("h3"));
